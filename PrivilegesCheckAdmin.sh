f27dc81dd8426e964d6eb3e9e7822c357397e0af
Jennifer Johnson <technotica@gmail.com>
2020-05-21 12:30:33 -0500

    Added check to if user is set as standard.

--- PrivilegesCheckAdmin.sh
+++ PrivilegesCheckAdmin.sh
@@ -11,7 +11,8 @@
 # #
 # Written by: Jennifer Johnson
 # Original concept drawn from:  TravelingTechGuy (https://github.com/TravellingTechGuy/privileges)
-# and Krypted (https://github.com/jamf/MakeMeAnAdmin)
+# and Krypted (https://github.com/jamf/MakeMeAnAdmin) 
+# and soundsnw (https://github.com/soundsnw/mac-sysadmin-resources/tree/master/scripts)
 #
 # Version: 0.1
 # Date: 5/19/20 
@@ -21,30 +22,30 @@
 
 # Indicate how long a user should have admin via Privileges.app, in minutes
 # Make this a plist so it can be selected?
-privilegesMinutes=5
+privilegesMinutes=2
+privilegeSeconds="$((privilegeMinutes * 60))"
 
 # Set date for the logs, this can be modified as dd.mm.yyyy or dd-mm-yyy
 DATE=$(/bin/date +"%d.%m.%Y")
 
-# Check for presence of a log file 
-logFile="/private/var/privileges/${loggedInUser}_${DATE}/.lastAdminCheck.txt"
-timeStamp=$(date +%s)
+# Grab the logged in user
+loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 
 #Read the User associated with being allowed to use Privileges.app
 #sudo defaults read "Path to configuration profile that sets LimitToUser"
 #"Set this variable to $AllowedUser"
 
-# Grab the logged in user
-loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-ed logs
+# Set log file location
+logFile="/private/var/privileges/${loggedInUser}_${DATE}/.lastAdminCheck.txt"
+timeStamp=$(date +%s)
 
 # Check if log file exists and set 
-
 if [ -f $logFile ]; then
  
 	  echo "File ${logFile} exists."
 		
 else
+
  	echo "File ${logFile} does NOT exist"
  	# Create a directory to drop collect logs
 	mkdir -p "/private/var/privileges/${loggedInUser}_${DATE}/"
@@ -54,20 +55,30 @@ else
   	echo "Creating admin timestamp"
   	touch "/usr/local/tatime"
 	chmod 600 "/usr/local/tatime"  
-	
+
 fi	
 
 # If no current user is logged in, exit quietly
 if [[ -z "$loggedInUser" ]]; then
+
     echo "No user logged in, exiting"
     exit 0
-fi
 
+fi
 
 # If timestamp file is not present, exit quietly 
 if [[ ! -e /usr/local/tatime ]]; then
+
     echo "No timestamp, exiting."
     exit 0
+
+fi
+
+# If user is a standard user, exit quietly
+if dseditgroup -o checkmember -m "$LoggedInUser" admin; then
+	echo "$loggedInUser is an admin."
+else
+	exit 0
 fi
 
 # Get current Unix time
@@ -82,23 +93,19 @@ echo "Unix time when admin was given: ""setTimeStamp"
 timeSinceAdmin="$((currentEpoch - setTimeStamp))"
 echo "Seconds since admin was given: ""$timeSinceAdmin"
 
-privilegeSeconds="$((privilegeMinutes * 60))"
-
 # If timestamp exists and the specified time has passed, remove admin
-
 if [[ -e /usr/local/tatime ]] && [[ (( timeSinceAdmin -gt privilegeSeconds )) ]]; then
 
     echo ""$privilegesMinutes" have passed, removing admin"
 	echo "Removing $loggedInUser's admin privileges"
+	/usr/local/bin/jamf displayMessage -message "Admin rights have been revoked."
 	# Demote the user using PrivilegesCLI  
 	sudo -u $loggedInUser /Applications/Privileges.app/Contents/Resources/PrivilegesCLI --remove
 	# Pull logs of what the user did. 
-	log collect --last 30m --output /private/var/privileges/${loggedInUser}_${DATE}/$loggedInUser-$setTimeStamp.logarchive
-    # Make sure timestamp file is not present and run recon
+	log collect --last 30m --output /private/var/privileges/${loggedInUser}_${DATE}/$setTimeStamp.logarchive
+    # Make sure timestamp file is not present
     mv -vf /usr/local/tatime /usr/local/tatime.old
-    /usr/local/jamf/bin/jamf recon
 
 fi
-
 
 exit 0

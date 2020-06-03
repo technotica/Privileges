e0287850c8a34be56f3fc37d9300717568964ef1
Jennifer Johnson <technotica@gmail.com>
2020-06-03 11:16:50 -0500

    Cleanup, 1.x release
    Changed admin expiration from using jamf displaymessage to Jamf Helper.
    
    Re-arranged check for loggedInUser

--- PrivilegesCheckAdmin.sh
+++ PrivilegesCheckAdmin.sh
@@ -14,8 +14,8 @@
 # and Krypted (https://github.com/jamf/MakeMeAnAdmin) 
 # and soundsnw (https://github.com/soundsnw/mac-sysadmin-resources/tree/master/scripts)
 #
-# Version: 0.4
-# Date: 6/1/20 
+# Version: 1.0
+# Date: 6/3/20 
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
@@ -29,7 +29,7 @@ loggedInUser=$(python -c "from Foundation import CFPreferencesCopyAppValue; prin
 # Determine how long a user should have admin via Privileges.app, in minutes from configuration profile.
 privilegesMinutes=$(python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('TimeLimit', 'edu.iastate.demote.privileges')")
 
-# Determine if Local logging enabled from configuration profile.
+# Determine if local logging is enabled from configuration profile.
 LocalLogging=$(python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('EnableLocalLog', 'edu.iastate.demote.privileges')")
 
 # Determine if Jamf custom trigger is set from configuration profile.
@@ -47,11 +47,21 @@ if [[ -z "$privilegesMinutes" ]]; then
 
 fi
 
-# If no current user is logged in, exit quietly
-if [[ -z "$loggedInUser" ]]; then
+# If user is not specified from configuration profile (e.g. it is blank) use the logged in user instead.
+ if [[ -z "$loggedInUser" ]]; then
 
-    echo "No user logged in, exiting"
+   # Otherwise grab the currently logged in user instead
+	loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+    echo "User not specified from configuration profile. $loggedInUser is logged in."
     exit 0
+
+    # If no current user is logged in, exit quietly
+	if [[ -z "$loggedInUser" ]]; then
+
+   		echo "No user logged in, exiting"
+   		exit 0
+
+	fi
 
 fi
 
@@ -66,19 +76,6 @@ else
 	#echo "$loggedInUser is a standard user."
 	userType="Standard"
 	exit 0
-
-fi
-
-# If user is not specified from configuration profile and is set to blank, exit quietly
- if [[ -z "$loggedInUser" ]]; then
-
-   # Otherwise grab the currently logged in user instead
-	loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-    echo "User $loggedInUser is logged in."
-#    # User shouldn't be able to promote themselves using Privileges.app.  Something horrible happened.  Give the user a prompt to let them know admin has been removed.
-#	 /usr/local/bin/jamf displayMessage -message "You shouldn't have been able to do this. Admin privileges have been revoked."
-#    sudo -u $loggedInUser /Applications/Privileges.app/Contents/Resources/PrivilegesCLI --remove
-   exit 0
 
 fi
 
@@ -108,8 +105,8 @@ if [[ ! -e "$logFile" ]] && [[ $userType = "Admin" ]]; then
 
 fi	
 
-# Check if our logfile exists (e.g. it got created when a user was admin and if the user used Privileges to demote themselves manually). 
-# this clears any of our timestamps and logs for better user experience next time the user launches Privileges.
+# Check if our logfile exists, e.g. it got created when a user was admin and if the user used Privileges to demote themselves manually.
+# This will clear any of our timestamps and logs for better user experience next time the user launches Privileges.
 
 if [[ -f "$logFile" ]] && [[ $userType = "Standard" ]]; then
  
@@ -147,13 +144,6 @@ privilegesSeconds="$((privilegesMinutes * 60))"
 if [[ -e /usr/local/tatime ]] && [[ (( timeSinceAdmin -gt privilegesSeconds )) ]]; then
 
     echo ""$privilegesMinutes" minutes have passed, removing admin privileges for $loggedInUser"
-
-    # Give the user a prompt to let them know admin has been removed.
-	/usr/local/bin/jamf displayMessage -message "Over $privilegesMinutes minutes has passed. Admin privileges have been removed."
-
-	# Use macOS Notification to let user know admin has been removed. 
-	# Won't work in macOS 10.14 or higher due to privacy rules.  Would need to allow Terminal full access for this to work.
-	#sudo -u $loggedInUser osascript -e 'display notification "Admin privileges have been removed." with title "Privileges"'
 
 	# Use JamfHelper to let user know admin has been removed via dialog box.
 	/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -icon /Applications/Privileges.app/Contents/Resources/AppIcon.icns -title "Privileges" -heading "Administrator Access" -description "Over $privilegesMinutes minutes has passed. Admin privileges have been removed." -button1 "OK" 

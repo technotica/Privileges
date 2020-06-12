326d67474e15237c14cf61be08a04b46f417a17a
Jennifer Johnson <technotica@gmail.com>
2020-06-12 17:36:21 -0500

    Add mobilconfig, cleanup
    Changed demote (line 132) to occur before warning message so demotion isn't waiting on user to click OK.  Force removal of debug logs so they don't get ginormous.

--- PrivilegesCheckAdmin.sh
+++ PrivilegesCheckAdmin.sh
@@ -14,8 +14,8 @@
 # and Krypted (https://github.com/jamf/MakeMeAnAdmin) 
 # and soundsnw (https://github.com/soundsnw/mac-sysadmin-resources/tree/master/scripts)
 #
-# Version: 1.1
-# Date: 6/8/20 
+# Version: 1.2
+# Date: 6/12/20 
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
@@ -36,11 +36,10 @@ LocalLogging=$(python -c "from Foundation import CFPreferencesCopyAppValue; prin
 CustomTrigger=$(python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('CustomTrigger', 'edu.iastate.demote.privileges')")
 
 # Set location of script logs for debugging
-DebugLogs="/private/var/logs/privilegesout.log"
+DebugLogs="/private/var/log/privilegesout.log"
 
 
 # If time limit before admin is promoted isn't set, then use default time of 20 minutes.  If this is not set, it will grep as None
-#if [[ -z "$privilegesMinutes" ]]; then
 if [[ $privilegesMinutes = "None" ]]; then
 
     #echo "Admin timeout not specified, using default of 20 minutes"
@@ -50,26 +49,12 @@ fi
 
 # If user is not specified in configuration profile (e.g. it is blank), then we don't care what user is logged in. 
 # We don't want to use the currently logged in user, in case it doesn't match what was set in the configuration profile because it may set user account that is admin to a standard account
-
-# If user is not specified from configuration profile (e.g. it is blank) use the logged in user instead.
-#if [[ -z "$loggedInUser" ]]; then
 if [[ $loggedInUser = "None" ]]; then
 
-#   echo "User not specified from configuration profile. $loggedInUser is logged in."
+#   echo "User not specified from configuration profile."
 	exit 0
 
 fi
-# Otherwise grab the currently logged in user instead
-# loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-
-
-# If no current user is logged in, exit quietly
-#	if [[ -z "$loggedInUser" ]]; then
-#	echo "No user is logged in."	
-#   exit 0
-#	fi
-
-#fi
 
 # If user an admin or standard.  If standard user, exit quietly
 if [[ $("/usr/sbin/dseditgroup" -o checkmember -m $loggedInUser admin / 2>&1) =~ "yes" ]]; then
@@ -92,19 +77,12 @@ timeStamp=$(date +%s)
 # If log file is NOT present and user is promoted to admin, then create a log file and our time stamps for time calculations
 if [[ ! -e "$logFile" ]] && [[ $userType = "Admin" ]]; then
 	
-	# Cleanup any old script logs left behind from last run, if still there.
-	if  [[ -e "$DebugLogs" ]]; then
-		
-		rm $DebugLogs
-
-	fi
-
  	echo "File ${logFile} does NOT exist"
  	# Create a directory to drop collect logs
 	mkdir -p "/private/var/privileges/${loggedInUser}_${DATE}/"
   	touch $logFile
  	echo $timeStamp > $logFile
-  	# Setup an initial time stamp when privileges was launched. It will be used to determine how long a user has been promoted to admin.
+  	# Setup an initial time stamp when Privileges was launched. It will be used to determine how long a user has been promoted to admin.
   	echo "Creating admin timestamp"
   	touch "/usr/local/tatime"
 	chmod 600 "/usr/local/tatime"  
@@ -113,13 +91,13 @@ fi
 
 # Check if our logfile exists, e.g. it got created when a user was admin and if the user used Privileges to demote themselves manually.
 # This will clear any of our timestamps and logs for better user experience next time the user launches Privileges.
-
 if [[ -f "$logFile" ]] && [[ $userType = "Standard" ]]; then
  
  	echo "File ${logFile} does exist"
 	# Make sure timestamp file is not present
     mv -vf /usr/local/tatime /usr/local/tatime.old
     rm $logFile
+    rm $DebugLogs
     exit 0
 fi	
 
@@ -151,11 +129,15 @@ if [[ -e /usr/local/tatime ]] && [[ (( timeSinceAdmin -gt privilegesSeconds )) ]
 
     echo ""$privilegesMinutes" minutes have passed, removing admin privileges for $loggedInUser"
 
+    # Demote the user using PrivilegesCLI  
+	sudo -u $loggedInUser /Applications/Privileges.app/Contents/Resources/PrivilegesCLI --remove
+
 	# Use JamfHelper to let user know admin has been removed via dialog box.
 	/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -icon /Applications/Privileges.app/Contents/Resources/AppIcon.icns -title "Privileges" -heading "Administrator Access" -description "Over $privilegesMinutes minutes has passed. Admin privileges have been removed." -button1 "OK" 
 
-	# Demote the user using PrivilegesCLI  
-	sudo -u $loggedInUser /Applications/Privileges.app/Contents/Resources/PrivilegesCLI --remove
+    # Make sure timestamp file is not present
+    mv -vf /usr/local/tatime /usr/local/tatime.old
+    rm $logFile
 
 	# Pull logs of what the user did during the time they were allowed admin rights.
 	if [[ $LocalLogging = "true" ]]; then
@@ -168,16 +150,18 @@ if [[ -e /usr/local/tatime ]] && [[ (( timeSinceAdmin -gt privilegesSeconds )) ]
 	fi
 	
 	# Send a custom Jamf trigger to a policy so we know someone used Privileges successfully, if configured.
-	if [[ $CustomTrigger !="None" ]]; then
-	#if [[ ! -e "$CustomTrigger" ]]; then
+	if [[ $CustomTrigger != "None" ]]; then
 
 		/usr/local/jamf/bin/jamf policy -event "$CustomTrigger"
 
 	fi
 
-    # Make sure timestamp file is not present
-    mv -vf /usr/local/tatime /usr/local/tatime.old
-    rm $logFile
+   	# Cleanup any old logs left over from running this script, if they're still there.
+	if  [[ -e "$DebugLogs" ]]; then
+		
+		rm $DebugLogs
+
+	fi
 
 fi
 
